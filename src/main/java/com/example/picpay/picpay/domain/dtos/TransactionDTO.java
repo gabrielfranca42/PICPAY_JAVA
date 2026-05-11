e816cb9f92b7a3fc4789e33521cc8f185956a754
BILLY BAT <gabrielfranca172@gmail.com>
2026-05-11 11:03:43 -0300

    ajeitando identação

--- src/main/java/com/example/picpay/picpay/domain/dtos/TransactionDTO.java
+++ src/main/java/com/example/picpay/picpay/domain/dtos/TransactionDTO.java
@@ -1,7 +1,6 @@
 package com.example.picpay.picpay.domain.dtos;
 
-
 import java.math.BigDecimal;
 
-public class TransactionDTO (BigDecimal value, Long senderId, Long receiverId) {
+public record TransactionDTO(BigDecimal value, Long senderId, Long receiverId) {
 }
