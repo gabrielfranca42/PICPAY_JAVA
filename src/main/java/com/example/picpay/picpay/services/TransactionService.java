ab56b248b8c689aab6014818545f7e73e26a3d5e
BILLY BAT <gabrielfranca172@gmail.com>
2026-05-12 10:16:33 -0300

    service feito ainda falta deixar decente

--- src/main/java/com/example/picpay/picpay/services/TransactionService.java
+++ src/main/java/com/example/picpay/picpay/services/TransactionService.java
@@ -5,9 +5,15 @@ import com.example.picpay.picpay.domain.transaction.Transaction;
 import com.example.picpay.picpay.domain.user.User;
 import com.example.picpay.picpay.repositories.TransactionRepository;
 import com.example.picpay.picpay.repositories.UserRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 @Service
 public class TransactionService {
@@ -25,7 +31,20 @@ public class TransactionService {
         User receiver = this.userService.findUserById(transaction.receiverId());
 
         userService.validateTransaction(sender, transaction.value());
+
+        boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
+        if(!isAuthorized){
+            throw new  Exception("transação deu merda ");
+        }
     }
 
+    public boolean authorizeTransaction(User sender, BigDecimal value) {
+        ResponseEntity<Map> autorizationResponse = restTemplate.getForEntity("https//run.mocky.io/v3/8fafdd68-a090-496f-8c9a-344cf30dae6", Map.class);
 
+        if (autorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) autorizationResponse.getBody().get("message");
+            return true;
+        } else return false;
+
+    }
 }
